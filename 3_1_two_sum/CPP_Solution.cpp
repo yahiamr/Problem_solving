fe327d4303a5c1c7f44d0a89ef68093016380555
Yahia Abukhalil <yahiamrvc@gmail.com>
2023-12-19 22:30:20 +0200

    Implemented solution for two sum problem

--- 3_1_two_sum/CPP_Solution.cpp
+++ 3_1_two_sum/CPP_Solution.cpp
@@ -1,15 +1,35 @@
 #include <vector>
-
+#include <unordered_map>
 using namespace std;
 
 
 class Solution {
 public:
  vector<int> twoSum(vector<int>& nums, int target) {
-    vector<int> ret;
+        int i, index_val;
+        vector<int> ret;
+        unordered_multimap<int,int> lst_map;
+        
+    
+        for(i= 0;i<nums.size();i++){
+            lst_map.insert(make_pair(nums[i],i));
+        }
 
+for (auto x : lst_map)
+        for(i= 0;i<nums.size();i++){
+        auto index = lst_map.find(target-nums[i]);
+
+            if (index != lst_map.end()&& index->second != i){
+            
+                 index_val = index->second;
+                ret.push_back(i);
+                ret.push_back(index_val);
+                
+                return ret; 
+            }
+        }
     return ret;
- }
+    }
 };
 
 
