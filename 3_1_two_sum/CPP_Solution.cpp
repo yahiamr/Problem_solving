a6b8248bfea351c1431643e2ac4be816b6e916d4
Yahia Abukhalil <yahiamrvc@gmail.com>
2023-12-19 22:25:21 +0200

    added solution class for two sum

---
+++ 3_1_two_sum/CPP_Solution.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+ vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> ret;
+
+    return ret;
+ }
+};
+
+
+int main(){
+   
+
+    return 0;
+}
