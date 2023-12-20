944c868fa44c576461a769b36bde25853d9efaaf
Yahia Abukhalil <yahiamrvc@gmail.com>
2023-12-20 23:09:04 +0200

    Added catch2 support for the first problem

--- 1_217_contains_duplicate/CPP_Solution.cpp
+++ 1_217_contains_duplicate/CPP_Solution.cpp
@@ -1,5 +1,7 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include <set>
 #include <vector>
+#include "catch.hpp"
 using namespace std;
 class Solution {
 public:
@@ -15,11 +17,18 @@ public:
     }*/
 };
 
-int main(){
+TEST_CASE("Duplicate Detection") {
     Solution sol;
-    vector<int> test1 = {1,2,3,1};
-    vector<int> test2 = {1,2,3,4};
-    vector<int> test3 = {1,1,1,3,3,4,3,2,4,2};
-    sol.containsDuplicate(test1);
-    return 0;
+
+    SECTION("Duplicates Present") {
+        std::vector<int> test1 = {1, 2, 3, 1};
+        std::vector<int> test3 = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+        REQUIRE(sol.containsDuplicate(test1) == true);
+        REQUIRE(sol.containsDuplicate(test3) == true);
+    }
+
+    SECTION("No Duplicates") {
+        std::vector<int> test2 = {1, 2, 3, 4};
+        REQUIRE(sol.containsDuplicate(test2) == false);
+    }
 }
